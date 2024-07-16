d1c6bcc2d0c68baa7d0c7ae39a6321c1ec5a1b4a
yktn-dd08 <yuh.kitano@gmail.com>
2024-07-17 07:23:38 +0900

    tentative vgg_cnn.py

---
+++ model/vgg_cnn.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+
+
+CFGS = {
+    "A": [64, "M", 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "B": [64, 64, "M", 128, 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "D": [64, 64, "M", 128, 128, "M", 256, 256, 256, "M", 512, 512, 512, "M", 512, 512, 512, "M"],
+    "E": [64, 64, "M", 128, 128, "M", 256, 256, 256, 256, "M", 512, 512, 512, 512, "M", 512, 512, 512, 512, "M"],
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, cfgs, in_channels, batch_norm=False, dropout=0.5, init_weights=True):
+        super(VGG, self).__init__()
+        self.features = VGG._make_layers(cfgs, in_channels, batch_norm)
+        self.avepool = nn.AdaptiveAvgPool2d((7, 7))
+        self.regressor = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(dropout),
+            nn.Linear(4096, 1)
+        )
+
+        if init_weights:
+            self._init_weight()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = self.avepool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+
+        return x
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+    @staticmethod
+    def _make_layers(self, cfg, channels, batch_norm):
+        layers = []
+        in_channels = channels
+        for v in cfg:
+            if v == "M":
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+                if batch_norm:
+                    layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(True)]
+                else:
+                    layers += [conv2d, nn.ReLU(True)]
+                in_channels = v
+
+        return nn.Sequential(*layers)
